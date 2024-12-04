8b2de39b5d38a74ee8719dee4126b48d3647a5b0
satviktyagi <tyagi2020satvik@gmail.com>
2024-12-04 14:25:23 -0500

    adds test script

--- utils.py
+++ utils.py
@@ -11,7 +11,6 @@ from transformers import (
 
 
 def load_model_tokenizer(model_name):
-    model_name = "facebook/bart-base"
     model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
     tokenizer = AutoTokenizer.from_pretrained(model_name)
     return model, tokenizer
@@ -39,9 +38,7 @@ def load_data(dataset_name="achrafothman/aslg_pc12"):
 
     print("\nSample data from train:")
     for i in range(3):
-        print(
-            "Gloss: " + train_data["gloss"][i] + "Rich text: " + train_data["text"][i]
-        )
+        print("Gloss: " + train_data["gloss"][i] + "Text: " + train_data["text"][i])
 
     return train_data, val_data, test_data
 
@@ -102,3 +99,44 @@ def compute_metrics(eval_preds, metric_name="rouge"):
     result["gen_len"] = np.mean(prediction_lens)
     result = {k: round(v, 4) for k, v in result.items()}
     return result
+
+
+def generate_rich_text(
+    test_data, model, tokenizer, encoder_max_length, compute_metrics=True
+):
+    inputs = tokenizer(
+        test_data["gloss"],
+        padding="max_length",
+        truncation=True,
+        max_length=encoder_max_length,
+        return_tensors="pt",
+    )
+    input_ids = inputs.input_ids.to(model.device)
+    attention_mask = inputs.attention_mask.to(model.device)
+    outputs = model.generate(input_ids, attention_mask=attention_mask)
+    output_str = tokenizer.batch_decode(outputs, skip_special_tokens=True)
+
+    outputs, output_str = generate_rich_text(
+        test_data, model, tokenizer, encoder_max_length
+    )
+    results = None
+    if compute_metrics:
+        preds, labels = postprocess_text(output_str, test_data["text"])
+
+        rouge = datasets.load_metric("rouge")
+        bleu = datasets.load_metric("bleu")
+
+        # Compute ROUGE
+        rouge_results = rouge.compute(
+            predictions=preds, references=labels, use_stemmer=True
+        )
+        rouge_results = {
+            key: value.mid.fmeasure * 100 for key, value in rouge_results.items()
+        }
+
+        # Compute BLEU
+        preds_tokens = [pred.split() for pred in preds]
+        labels_tokens = [[label.split()] for label in labels]
+        bleu_results = bleu.compute(predictions=preds_tokens, references=labels_tokens)
+        results = {"rouge": rouge_results, "bleu": bleu_results}
+    return outputs, output_str, results
